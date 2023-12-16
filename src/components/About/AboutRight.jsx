8b436e53984762b7e506eb4e18a9046c894f6b8b
ravi <ravibadami191@gmail.com>
2023-12-16 17:43:49 +0530

    Made About us section responsive

---
+++ src/components/About/AboutRight.jsx
@@ -0,0 +1,27 @@
+import HeroButton from "../Hero/HeroButton";
+
+const AboutRight = () => {
+  return (
+    <div className=" flex w-1/2  flex-col items-start   justify-center  md:h-[80%]  md:w-1/2 md:gap-8 ">
+      <div className="w-7/12   text-start">
+        <span className=" text-base font-bold md:text-7xl">
+          About us <span className=" text-red-600">TAJ</span>BITE
+        </span>
+      </div>
+      <div className=" h-24 w-[90%] overflow-clip  text-start md:h-auto md:w-auto">
+        <span className="text-[0.60rem] font-semibold   md:text-2xl">
+          At TajByte, indulge in a culinary journey where ancient Indian flavors
+          meet modern delights -
+          <span className="hidden md:block">
+            A perfect fusion of Vedic cuisine, pizza, and sweet treats.
+          </span>
+        </span>
+      </div>
+      <div className="-ml-1 -mt-4 md:-ml-0 md:-mt-0">
+        <HeroButton button={"More"} />
+      </div>
+    </div>
+  );
+};
+
+export default AboutRight;
--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,36 +1,12 @@
+import AboutLeft from "./AboutLeft";
+import AboutRight from "./AboutRight";
+
 const AboutUs = () => {
   return (
     <div>
-      <div className="flex h-[90vh] items-center justify-around  p-5 px-16 pl-10">
-        {/** Left */}
-        <div className="h-[80%] w-1/2 ">
-          <img
-            className="h-[100%] w-11/12 rounded-[60px]"
-            src=" https://i.postimg.cc/DyYQd6J9/pixlr-image-generator-56ecd33b-1e1f-4d1a-9805-e41153bb03d7.png"
-            alt=""
-          />
-        </div>
-
-        {/** Right */}
-        <div className=" flex h-[80%] w-1/2   flex-col items-start justify-center gap-8 ">
-          <div className="w-7/12  text-start">
-            <span className=" text-7xl font-bold">
-              About us <span className=" text-red-600">TAJ</span>BITE
-            </span>
-          </div>
-          <div className=" text-start">
-            <span className="text-2xl font-semibold">
-              At TajByte, indulge in a culinary journey where ancient Indian
-              flavors meet modern delights – a perfect fusion of Vedic cuisine,
-              pizza, and sweet treats.
-            </span>
-          </div>
-          <div className="">
-            <button className="  rounded-full  bg-[#F04949] px-7 py-3 font-medium text-white drop-shadow-xl">
-              More
-            </button>
-          </div>
-        </div>
+      <div className=" flex h-[30vh] items-center justify-around gap-3 border  border-black pl-2 md:h-[90vh] md:p-5 md:px-16 md:pl-10">
+        <AboutLeft />
+        <AboutRight />
       </div>
     </div>
   );
