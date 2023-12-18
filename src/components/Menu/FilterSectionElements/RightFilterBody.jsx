3149c2c37cb52beabf63cba162a651cfd344b5fc
ravi <ravibadami191@gmail.com>
2023-12-18 17:55:08 +0530

    FIlter section logic completed

--- src/components/Menu/FilterSectionElements/RightFilterBody.jsx
+++ src/components/Menu/FilterSectionElements/RightFilterBody.jsx
@@ -2,17 +2,27 @@ import { useSelector } from "react-redux";
 import CuisinesSection from "./CuisinesSection/CuisinesSection";
 import RatingSection from "./RatingsSection/RatingSection";
 import SortSection from "./SortSection/SortSection";
+import { useState } from "react";
 
 const RightFilterBody = () => {
   const result = useSelector((store) => store.filter.cusineButton);
+  const [a, handleButton] = useState(0);
   if (!result) return;
   const ravi = result;
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleButton((prev) => prev + 1);
+  };
+
   return (
     <div className="w-9/12 border-t-2 p-4 pl-10">
-      {ravi === "Sort" && <SortSection />}
-      {ravi === "Cuisines" && <CuisinesSection />}
-      {ravi === "Ratings" && <RatingSection />}
+      <form action="" onSubmit={submit}>
+        {ravi === "Sort" && <SortSection a={a} />}
+        {ravi === "Cuisines" && <CuisinesSection a={a} />}
+        {ravi === "Ratings" && <RatingSection a={a} />}
+      </form>
+      <button onClick={submit}>submit</button>
     </div>
   );
 };
--- src/components/Menu/FilterSectionElements/SortSection/SortSection.jsx
+++ src/components/Menu/FilterSectionElements/SortSection/SortSection.jsx
@@ -3,18 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateSort } from "../../../../utils/redux/filterSlice";
 
 const SortSection = () => {
-  const [selectedOption, setSelectedOption] = useState("Relevance");
   const dispatch = useDispatch();
   const result = useSelector((store) => store.filter.sort);
-  console.log(result);
+  const [selectedOption, setSelectedOption] = useState(result);
 
   const handleSelectedOption = (event) => {
     setSelectedOption(event.target.value);
   };
   const handleSubmit = (m) => {
-    dispatch(updateSort(selectedOption));
     setSelectedOption(m);
+    dispatch(updateSort(selectedOption));
   };
+
   const dataArray = [
     "Relevance",
     "Rating",
@@ -24,25 +24,24 @@ const SortSection = () => {
   return (
     <div className="">
       <p> SORT BY</p>
-      <form action="" onSubmit={(e) => e.preventDefault()}>
-        {dataArray.map((m) => (
-          <div
-            className="mt-2  flex w-[50.7%] cursor-pointer   "
-            key={m}
-            onClick={() => handleSubmit(m)}
-          >
-            <input
-              type="radio"
-              value={m}
-              checked={selectedOption === m}
-              name="filter"
-              onChange={handleSelectedOption}
-            />
-            <span className="ml-2 cursor-pointer  select-none">{m}</span>
-          </div>
-        ))}
-        <button onClick={handleSubmit}>submit</button>
-      </form>
+
+      {dataArray.map((m) => (
+        <div
+          className="mt-2  flex w-[50.7%] cursor-pointer   "
+          key={m}
+          onClick={() => handleSubmit(m)}
+        >
+          <input
+            type="radio"
+            value={m}
+            checked={selectedOption === m}
+            name="filter"
+            onChange={handleSelectedOption}
+          />
+          <span className="ml-2 cursor-pointer  select-none">{m}</span>
+        </div>
+      ))}
+      {/* <button onClick={handleButton}>submit</button> */}
     </div>
   );
 };
