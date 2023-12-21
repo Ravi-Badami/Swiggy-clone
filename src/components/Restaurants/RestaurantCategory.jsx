bca0d8d82762a203e55dd6107457c05159b5cedc
ravi <ravibadami191@gmail.com>
2023-12-21 23:29:25 +0530

    Restaurant menu responsive

--- src/components/Restaurants/RestaurantCategory.jsx
+++ src/components/Restaurants/RestaurantCategory.jsx
@@ -8,20 +8,26 @@ const RestaurantCategory = ({ data, showList, setShowList }) => {
   };
 
   return (
-    <div className=" m-2 mx-auto w-6/12 cursor-pointer border-b-4  p-2 px-3 shadow-2xl ">
+    <div className=" m-2 mx-auto w-[75%] cursor-pointer border-b-4 p-1  px-3 shadow-2xl md:w-6/12 ">
       <div
-        className={` flex justify-between  hover:shadow-sm hover:shadow-gray-600 hover:drop-shadow-md ${
+        className={` flex h-full  justify-between hover:shadow-sm hover:shadow-gray-600 hover:drop-shadow-md ${
           showList
             ? "  bg-[#F04949] font-medium text-white shadow-sm shadow-gray-600 drop-shadow-md"
             : " hover:bg-white hover:text-black"
         }   rounded-full  p-2 px-4 hover:bg-[#F04949] hover:font-medium `}
         onClick={handleClick}
       >
-        <div className="flex">
-          <div className=" select-none">{data?.title}</div>
-          <div className="  select-none">({data.itemCards.length})</div>
+        <div className="flex items-center">
+          <div className=" select-none text-[0.72rem] md:text-base ">
+            {data?.title}
+          </div>
+          <div className="  select-none text-[0.72rem] md:text-base">
+            ({data.itemCards.length})
+          </div>
         </div>
-        <div className=" select-none">{showList ? "☝️" : "👇"}</div>
+        <div className=" select-none text-[0.72rem] md:text-base">
+          {showList ? "☝️" : "👇"}
+        </div>
       </div>
       {showList && <ItemList data={data.itemCards} />}
     </div>
