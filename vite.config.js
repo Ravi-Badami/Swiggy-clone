cf2d14006897b4602cea79a704139c9b5292ac51
ravi <ravibadami191@gmail.com>
2023-12-22 08:42:44 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,7 @@ export default defineConfig({
     proxy: {
       "/dapi": "https://www.swiggy.com",
       changeOrigin: true,
-      rewrite: (path) =>
-        path.replace(
-          /^\/dapi/,
-          "https://tajbite-fet2ek3ch-ravikumars-projects-4a656752.vercel.app",
-        ),
+      rewrite: (path) => path.replace(/^\/dapi/, "https://tajbite.vercel.app/"),
       // "/mapi": "https://www.swiggy.com",
     },
   },
