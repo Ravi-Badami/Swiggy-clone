3cb17091f92ba4c20eee68785ff9c2bfef6e756f
ravi <ravibadami191@gmail.com>
2023-12-22 08:29:27 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       rewrite: (path) =>
         path.replace(
           /^\/dapi/,
-          " https://https://tajbite-g65c51z7a-ravikumars-projects-4a656752.vercel.app",
+          "https://tajbite-g65c51z7a-ravikumars-projects-4a656752.vercel.app",
         ),
       // "/mapi": "https://www.swiggy.com",
     },
