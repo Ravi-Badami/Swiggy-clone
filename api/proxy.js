3a498b06f404729a7e2b1e1b522db97b6014092b
ravi <ravibadami191@gmail.com>
2023-12-22 17:14:30 +0530

    updated vcc

--- api/proxy.js
+++ api/proxy.js
@@ -3,18 +3,24 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-  const url = `https://www.tajbite.vercel.app${req.url}`;
-  const response = await fetch(url, {
-    method: req.method,
-    headers: {
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers":
-        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    },
-  });
+  try {
+    const url = `https://www.tajbite.vercel.app${req.url}`;
+    const response = await fetch(url, {
+      method: req.method,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Accept, Content-Type",
+      },
+    });
 
-  const data = await response.json();
-  res.json(data);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
