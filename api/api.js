b5711e2906c12e13981719723d317e55f38345d0
ravi <ravibadami191@gmail.com>
2023-12-22 12:28:19 +0530

    vc updated

--- api/api.js
+++ api/api.js
@@ -1,6 +1,9 @@
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "https://tajbite.vercel.app/");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://www.tajbite.vercel.app/",
+  );
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
