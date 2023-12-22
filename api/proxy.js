978085c821cd6d3cec11b346c9ec4f3dfd19b206
ravi <ravibadami191@gmail.com>
2023-12-22 22:44:47 +0530

    hiden proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,13 @@
-// api/proxy.js
-import axios from "axios";
+// import axios from "axios";
 
-export default async function handler(req, res) {
-  try {
-    const response = await axios.get(
-      "https://tajbite.vercel.app/dapi/restaurants/list/v5?lat=15.3803485&lng=73.8349952&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-    );
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error proxying request:", error.message);
-    res.status(500).send("Internal Server Error");
-  }
-}
+// export default async function handler(req, res) {
+//   try {
+//     const response = await axios.get(
+//       "https://tajbite.vercel.app/dapi/restaurants/list/v5?lat=15.3803485&lng=73.8349952&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+//     );
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error("Error proxying request:", error.message);
+//     res.status(500).send("Internal Server Error");
+//   }
+// }
