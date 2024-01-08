d2a2132e7a6abfb3e91ad353dc562931290d1115
ravi <ravibadami191@gmail.com>
2024-01-08 20:24:55 +0530

    blue overlay on links and buttons solved

--- src/components/Header/SignUp.jsx
+++ src/components/Header/SignUp.jsx
@@ -6,7 +6,7 @@ const SignUp = () => {
   return (
     <div>
       <button
-        className="rounded-full  bg-[#F04949] px-4 py-1 font-normal text-white drop-shadow-xl md:px-6 md:py-2 md:font-medium"
+        className="rounded-full  bg-[#F04949] px-4 py-1 font-normal text-white outline-none drop-shadow-xl md:px-6 md:py-2  md:font-medium"
         onClick={() => {
           register === "login" ? setRegister("logout") : setRegister("login");
         }}
