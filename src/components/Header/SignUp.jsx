eaf707c7a7b25cf9078c94dd79978d77d0cd2db6
ravi <ravibadami191@gmail.com>
2024-01-13 15:24:32 +0530

    checking

--- src/components/Header/SignUp.jsx
+++ src/components/Header/SignUp.jsx
@@ -4,6 +4,7 @@ const SignUp = () => {
   const [register, setRegister] = useState("login");
 
   return (
+    //
     <div>
       <button
         className="rounded-full  bg-[#F04949] px-4 py-1 font-normal text-white outline-none drop-shadow-xl md:px-6 md:py-2  md:font-medium"
